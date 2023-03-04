b238dc544f1343bae4369e3206375ea846fb6294
vanhiep99w <vanhiep99w@gmail.com>
2023-03-04 20:25:53 +0700

    hallo-project html version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $('#slide').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        arrows: false,
+        dots: true
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      "dark-primary": "#000000",
+      "dark-secondary": "#B4B4B4",
+      "dark-tertiary": "#F1F1F1",
+      "blue-primary": "#3AADDE",
+      "red-primary": "#EF233C",
+    }
+  },
+  plugins: [],
+}
